b1a9895da4de9beedd181a343cadb6251f598923
distinkt-dd <dpemvr@yandex.ru>
2026-04-15 23:26:41 +0500

    feat(middlewares) добавил middleware для валидации zod

---
+++ src/middlewares/validate.middleware.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express'
+import { ZodError, ZodSchema } from 'zod/v3'
+
+export const validate = (schema: ZodSchema) => {
+	return async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			await schema.parseAsync(req.body)
+			next()
+		} catch (err) {
+			if (err instanceof ZodError) {
+				return res.status(400).json({
+					success: false,
+					errors: err.issues.map(err => ({
+						field: err.path.join('.'),
+						message: err.message
+					}))
+				})
+			}
+			next(err)
+		}
+	}
+}
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 
 import prisma from './prisma'
 
@@ -6,10 +6,20 @@ const { PORT = 3000 } = process.env
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	console.error(err.stack)
+	res.status(500).json({
+		message: 'Что то пошло не так!',
+		error: err.message
+	})
+})
 
 async function server() {
 	await prisma.$connect()
-	console.log('Сервер активен!')
+	console.log('База данных активна!')
 	app.listen(PORT)
 	console.log(`Сервер запущен на http://localhost:${PORT}`)
 }
